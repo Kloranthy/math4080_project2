f0ae6cc1ac59e25957f57497d42a1aa005e9cce2
Josh Weigel <jdweigel@gmail.com>
2020-05-03 01:15:07 -0400

    Updates

---
+++ precache-manifest.51d38f6fcec304f5fc8432f92ae7011d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21ebc1055e734152d29ab2925d455a76",
+    "url": "/math4080_project2/index.html"
+  },
+  {
+    "revision": "46f821390fdc61177509",
+    "url": "/math4080_project2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "085cc6a7d78a18b2161c",
+    "url": "/math4080_project2/static/js/2.ca2522ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/math4080_project2/static/js/2.ca2522ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46f821390fdc61177509",
+    "url": "/math4080_project2/static/js/main.98fb1937.chunk.js"
+  },
+  {
+    "revision": "62cc80ef1bfccb645cef",
+    "url": "/math4080_project2/static/js/runtime-main.5b481768.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/math4080_project2/static/media/logo.5d5d9eef.svg"
+  }
+]);
